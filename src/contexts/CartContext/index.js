e9dc6476ef9b16fa561b8772db83ae112e0602ea
Diego Almeida <diegoalmda@gmail.com>
2021-07-22 00:12:35 -0300

    Delete some files

--- src/contexts/CartContext/index.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export const CartContext = createContext({});
-
-export const CartProvider = ({ children }) => {
-    // const showCart = children.show;
-    // return <CartContext.Provider value={{ showCart }}>{children}</CartContext.Provider>;
-}
